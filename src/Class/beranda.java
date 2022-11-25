50fb1cc9be1238b5c7163f7c0c76e92022e70eb7
tmalikkamal <tmalik.k22@gmail.com>
2022-11-25 15:14:15 +0700

    tambah class beranda dan pesanan

---
+++ src/Class/beranda.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+/**
+ *
+ * @author USER DK
+ */
+public class beranda {
+    private String Asal;
+    private String Tujuan;
+    private String Tanggal_pemesanan;
+    private String Jumlah_kursi;
+
+    public String getAsal() {
+        return Asal;
+    }
+
+    public void setAsal(String asal) {
+        Asal = asal;
+    }
+
+    public String getTujuan() {
+        return Tujuan;
+    }
+
+    public void setTujuan(String tujuan) {
+        Tujuan = tujuan;
+    }
+
+    public String getTanggal_pemesanan() {
+        return Tanggal_pemesanan;
+    }
+
+    public void setTanggal_pemesanan(String tanggal_pemesanan) {
+        Tanggal_pemesanan = tanggal_pemesanan;
+    }
+
+    public String getJumlah_kursi() {
+        return Jumlah_kursi;
+    }
+
+    public void setJumlah_kursi(String jumlah_kursi) {
+        Jumlah_kursi = jumlah_kursi;
+    }
+}
